af9739115a414939bc900fc0e33a30bc900af8c0
Bautistagl <bautistagonzalezlazo@gmail.com>
2023-07-14 17:58:58 -0300

    dsadas

--- rodamientos.bb/src/pages/busquedaAltura.js
+++ rodamientos.bb/src/pages/busquedaAltura.js
@@ -138,77 +138,127 @@ export default function BusquedaAltura() {
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
-    <div className="fondo-busqueda">
-      <>.</>
-      <div className="barra-busqueda">
-      <Image className='icono-busqueda' width={30} height={30} alt='' src='/iconoBusqueda.png'/>
-        <input
-          className='input-busqueda'
-          type="text"
-          value={searchTerm}
-          onChange={handleSearch}
-          placeholder="ALTURA"
-        />
-      </div>
+      <div className="fondo-busqueda">
+        <>.</>
 
-      {Object.keys(groupedResults).map((codigo1, index) => (
-        <div className="contenedor-cards" key={index}>
+        <div className="barra-busqueda">
+          {/* <span>Buscar producto por código:</span> */}
           <Image
-            style={{
-              marginTop: 'auto',
-              marginBottom: 'auto',
-              marginLeft: '20px',
-            }}
+            className="icono-busqueda"
+            width={30}
+            height={30}
             alt=""
-            src="/rodamiento.webp"
-            width={200}
-            height={200}
+            src="/iconoBusqueda.png"
           />
-          <div className="textos-completo">
-            <div className="codigo-medidas">
-              <div className="titulo-singular">{codigo1}</div>
-              <div className="medidas">
-                <span>Interior: {groupedResults[codigo1][0].interior}  mm</span>
-                <span>Exterior: {groupedResults[codigo1][0].exterior} mm</span>
-                <span>Altura: {groupedResults[codigo1][0].altura} mm</span>
-              </div>
-            </div>
-            <div className="contenedor-propiedades">
-              <div className="propiedades-principales">
-                <span>MARCA</span>
-                <span>PRECIO</span>
-                <span>STOCK</span>
-               
-              </div>
+          <input
+            className="input-busqueda"
+            type="text"
+            value={searchTerm}
+            onChange={handleSearch}
+            placeholder="CÓDIGO"
+          />
+        </div>
 
-              {groupedResults[codigo1].map((producto, marcaIndex) => (
-                <div className="propiedades" key={marcaIndex}>
-                  <Image
-                    style={{ marginRight: '40px' }}
-                    alt=""
-                    src={ producto.imagen === '' ? '/TEST.jpg' :`/${producto.imagen}.png`}
-                    width={200}
-                    height={20}
-                  />
-                  
-                  <span style={{ marginRight: '110px' }}>${producto.precio}</span>
-                  <span style={{ color: producto.stock === 'Disponible' ? 'green' : 'red' }}>
-                    {producto.stock}
-                  </span> 
-               
-
-               
+        {Object.keys(groupedResults).map((codigo1, index) => (
+          <div className="contenedor-cards" key={index}>
+            <Image
+              style={{
+                marginTop: 'auto',
+                marginBottom: 'auto',
+                marginLeft: '20px',
+              }}
+              alt=""
+              src="/rodamiento.webp"
+              width={200}
+              height={200}
+            />
+            <div className="textos-completo">
+              <div className="codigo-medidas">
+                <div className="titulo-singular">{codigo1}</div>
+                <div className="medidas">
+                  <span>
+                    Interior: {groupedResults[codigo1][0].interior} mm
+                  </span>
+                  <span>
+                    Exterior: {groupedResults[codigo1][0].exterior} mm
+                  </span>
+                  <span>Altura: {groupedResults[codigo1][0].altura} mm</span>
                 </div>
-              ))}
+              </div>
+              <div className="contenedor-propiedades">
+                <div className="propiedades-principales">
+                  <span>
+                    {' '}
+                    <Image
+                      alt=""
+                      style={{ marginRight: '10px', marginTop: '-5px' }}
+                      src="/tag.png"
+                      width={40}
+                      height={40}
+                    />
+                    MARCA
+                  </span>
+                  <span>
+                    {' '}
+                    <Image
+                      alt=""
+                      style={{ marginRight: '10px', marginTop: '-5px' }}
+                      src="/iconoPlata.png"
+                      width={40}
+                      height={40}
+                    />{' '}
+                    PRECIO
+                  </span>
+                  <span>
+                    {' '}
+                    <Image
+                      alt=""
+                      style={{ marginRight: '10px', marginTop: '-5px' }}
+                      src="/stock.png"
+                      width={30}
+                      height={30}
+                    />
+                    STOCK
+                  </span>
+                </div>
+
+                {groupedResults[codigo1].map((producto, marcaIndex) => (
+                  <div className="propiedades" key={marcaIndex}>
+                    <Image
+                      style={{ marginRight: '100px' }}
+                      alt=""
+                      src={
+                        producto.imagen === ''
+                          ? '/TEST.jpg'
+                          : `/${producto.imagen}.png`
+                      }
+                      width={100}
+                      height={25}
+                    />
+
+                    <span className="span-1">${producto.precio}</span>
+                    <span
+                      className="span-2"
+                      style={{
+                        fontWeight:'bold',
+                        color:
+                          producto.stock === 'Disponible'
+                            ? 'green'
+                            : producto.stock === 'No disponible'
+                            ? 'red'
+                            : 'rgb(215, 215, 58)',
+                      }}>
+                       { producto.stock ? (producto.stock).toUpperCase() : ''}
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
-    
-    </div>
-    
+        ))}
+      </div>
     </div>
   );
 
