fc10fc3f14d1d01e42098c050f220809cd0ff0c5
Bautistagl <bautistagonzalezlazo@gmail.com>
2024-01-19 14:42:37 -0300

    dsada

--- rodamientos.bb/src/pages/actualizarCsv.js
+++ rodamientos.bb/src/pages/actualizarCsv.js
@@ -86,7 +86,7 @@ export default function ExcelUpdater() {
 
   const handleAcceptChanges = async () => {
 
-    await removeSKFProducts();
+  
 
     if (csvData) {
       setStatus('Procesando archivo...');
